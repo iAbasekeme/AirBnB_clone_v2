55f06c15470b189d721fee5d8f635a43c9f3ceca
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-03 13:28:07 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,7 +17,7 @@ sudo echo "<html>
 # Remove existing symbolic link (if it exists)
 sudo rm -f /data/web_static/current
 # Create a new symbolic link to the latest release directory
-sudo ln -s /data/web_static/releases/test/ /data/web_static/etc/nginx/sites-available/default/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 # Give ownership to the ubuntu user and group recursively
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '8i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' 
