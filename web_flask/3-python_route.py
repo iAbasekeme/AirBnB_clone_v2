3e64b39e219bc7e4c3815a4ef1ddde76ff5cb64d
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-17 15:01:50 +0100

    Create 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""A script that runs a flask application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """A function that prints to stdout
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """A function that prints to stdout
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """A method that displays text
+    """
+    text = text.replace('_', ' ')
+    return f'C ' + text
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def display_kwargs(text='is cool'):
+    """A method that displays text
+    """
+    text = text.replace('_', ' ')
+    return f'C ' + text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
