a09e3dc8f4d435b2c753e1ad716354be41ccb674
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-17 15:42:45 +0100

    Create 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""A script that runs a flask application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """A function that prints to stdout
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """A function that prints to stdout
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """A method that displays text
+    """
+    text = text.replace('_', ' ')
+    return f'C ' + text
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_kwargs(text='is cool'):
+    """A method that displays text
+    """
+    text = text.replace('_', ' ')
+    return f'Python ' + text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def route_int(n):
+    """A method that prints only an int
+    """
+    return f'{n} is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
