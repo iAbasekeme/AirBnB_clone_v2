a31947b02ae521af48c1ae7b2b37599047690a92
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-17 19:19:04 +0100

    Create 6-number_odd_or_even.py

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""A script that runs a flask application
+"""
+from flask import render_template
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """A function that prints to stdout
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """A function that prints to stdout
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """A method that displays text
+    """
+    text = text.replace('_', ' ')
+    return f'C ' + text
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_kwargs(text='is cool'):
+    """A method that displays text
+    """
+    text = text.replace('_', ' ')
+    return f'Python ' + text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def route_int(n):
+    """A method that prints only an int
+    """
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_template(n):
+    """ A method that displays a html template
+    """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def disply_odd_even(n):
+    """ A method that displays a html template
+    """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
