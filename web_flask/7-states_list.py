bc8899dc635edf70301de9fd25da2bd1864d83ca
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-19 00:28:48 +0100

    Create 7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""A script that runs a flask application
+"""
+from flask import render_template
+from flask import Flask
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.after_request
+def remove_session(response):
+    """Remove the current SQLAlchemy session after each request
+    """
+    storage.close()
+    return response
+
+
+@app.teardown_appcontext
+def teardown_storage(exception):
+    """Teardown method to close storage after app context is destroyed
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def index():
+    """Route to display a HTML page with a list of states
+    """
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
