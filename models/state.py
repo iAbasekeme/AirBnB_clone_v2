45e7cdc26f62507b785574fbbecf4dde68ac702f
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-18 23:07:42 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -25,7 +25,7 @@ class State(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    if models.storageType != "db":
+    # if models.storageType != "db":
         """@property
         def cities(self):
             Getter method that gets a
@@ -35,12 +35,12 @@ class State(BaseModel, Base):
             citiesList = [city for city in allCities.values()
                         if city.state_id == self.id]
             return citiesList"""
-        if getenv("HBNB_TYPE_STORAGE") != "db":
-            @property
-            def cities(self):
-                """Get a list of all related City objects."""
-                city_list = []
-                for city in list(models.storage.all(City).values()):
-                    if city.state_id == self.id:
-                        city_list.append(city)
-                return city_list
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
