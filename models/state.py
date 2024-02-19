9873de38e28f53740b5384881a7dc8f18df44e6b
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-19 11:15:43 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -27,12 +27,12 @@ class State(BaseModel, Base):
     else:
         name = ""
 
-        @property
-        def cities(self):
-            """Get a list of all related City objects.
-            """
-            city_list = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    @property
+    def cities(self):
+        """Get a list of all related City objects.
+        """
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
