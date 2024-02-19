dfb829169a98e121111fdd19996e9ca5a175c521
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-02-19 11:22:14 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -27,12 +27,26 @@ class State(BaseModel, Base):
     else:
         name = ""
 
-    @property
-    def cities(self):
-        """Get a list of all related City objects.
-        """
-        city_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    if models.storageType != "db":
+        """@property
+        def cities(self):
+            Getter method that gets a
+            list of cities with the same stateid
+
+            allCities = models.storage.all(City)
+            citiesList = [city for city in allCities.values()
+                        if city.state_id == self.id]
+            return citiesList"""
+        if getenv("HBNB_TYPE_STORAGE") != "db":
+            @property
+            def cities(self):
+                """Get a list of all related City objects.
+                """
+                city_list = []
+                for city in models.storage.all(City).values():
+                    if city.state_id == self.id:
+                        city_list.append(city)
+                return city_list
